4540c20b61e04a6122f72b21313c61646ec4d9a4
Alexandr Khramov <iofakh@yandex.ru>
2023-02-05 02:27:59 +0300

    Final?

--- yatube/posts/tests/test_forms.py
+++ yatube/posts/tests/test_forms.py
@@ -3,7 +3,6 @@ import tempfile
 from http import HTTPStatus
 
 from django.conf import settings
-
 from django.core.cache import cache
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase, override_settings
